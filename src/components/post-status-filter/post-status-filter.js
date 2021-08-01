72315e744662fb41285bda8af2b94427f9c81e15
Abdulaziz <abdulazizfaactory@gmail.com>
2021-08-01 10:35:11 +0500

    to-do done

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Button} from 'reactstrap';
 import './post-status-filter.css'
 
-const PostStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            {/* <Button outline color='info'>All</Button> */}
-            <button type='button' className="btn btn-info">All posts</button> 
-            <button type='button' className="btn btn-outline-secondary">Liked</button>    
-        </div>
-    )
+export default class PostStatusFilter extends Component{
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            {name: 'all', label: 'All posts'},
+            {name: 'like', label: 'Liked'},
+        ]
+    }
+    render() {
+        const buttons = this.buttons.map(({name, label}) => {
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const activeClass = active ? 'btn-info': 'btn-outline-secondary';
+
+            return (
+                <button
+                    key={name}
+                    type='button'
+                    className={`btn ${activeClass}`}
+                    onClick={() => onFilterSelect(name)}>{label}</button>
+            );      
+        });
+
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+        // return (
+        //     <div className="btn-group">
+        //         {/* <Button outline color='info'>All</Button> */}
+        //         <button type='button' className="btn btn-info">All posts</button> 
+        //         <button type='button' className="btn btn-outline-secondary">Liked</button>    
+        //     </div>
+        // )
+    }
 }
 
-export default PostStatusFilter;
+// export default PostStatusFilter;
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,15 +1,47 @@
-import React from 'react';
+// import React from 'react';
+
+// import './search-panel.css'
+// // creating component
+// const SearchPanel = () => {
+//     return (
+//         <input
+//             className="form-control search-input"
+//             type="text"
+//             placeholder="Search from posts"
+//         />
+//     )
+// }
+
+// export default SearchPanel;
+import React, { Component } from 'react';
 
 import './search-panel.css'
 // creating component
-const SearchPanel = () => {
-    return (
-        <input
-            className="form-control search-input"
-            type="text"
-            placeholder="Search from posts"
-        />
-    )
+export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    }
+    onUpdateSearch(e) {
+        const term = e.target.value;
+        this.setState({
+            term
+        });
+        this.props.onUpdateSearch(term)
+    }
+
+    render() {
+        return (
+            <input
+                className="form-control search-input"
+                type="text"
+                placeholder="Search from posts"
+                onChange={this.onUpdateSearch}
+            />
+        )
+    }
 }
 
-export default SearchPanel;
